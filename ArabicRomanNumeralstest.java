0c11847d596501237b856e7adaf9539a65d59f6c
Ayoub <ayoubbourti3@gmail.com>
2024-12-18 21:58:22 +0100

    cycle 2

--- ArabicRomanNumeralstest.java
+++ ArabicRomanNumeralstest.java
@@ -1,17 +1,24 @@
 package nombres;
-
-
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ArabicRomanNumeralstest {
-    public class ArabicRomanNumeralstest {
+
         @Test
         public void convert_1_into_I() {
 
             int i = 1;
             String expected = "I";
+            ArabicRomanNumerals converter = new ArabicRomanNumerals();
+            String result = converter.convert(i);
+            assertThat(result).isEqualTo(expected);
+        }
+        @Test
+        public void convert_2_into_II() {
+
+            int i = 2;
+            String expected = "II";
             ArabicRomanNumerals converter = new ArabicRomanNumerals();
             String result = converter.convert(i);
             assertThat(result).isEqualTo(expected);
